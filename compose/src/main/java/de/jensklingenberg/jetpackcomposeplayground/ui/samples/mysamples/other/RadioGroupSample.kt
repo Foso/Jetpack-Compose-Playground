5f1ed411093fe5e00e56c5849e6311a5d311d748
Jens Klingenberg <mail@jensklingenberg.de>
2019-11-18 20:08:32 +0100

    add RadioGroup example

---
+++ compose/src/main/java/de/jensklingenberg/jetpackcomposeplayground/ui/samples/mysamples/other/RadioGroupSample.kt
@@ -0,0 +1,21 @@
+package de.jensklingenberg.jetpackcomposeplayground.ui.samples.mysamples.other
+
+import androidx.compose.Composable
+import androidx.compose.state
+import androidx.compose.unaryPlus
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.RadioGroup
+
+@Composable
+fun RadioGroupSample() {
+    MaterialTheme {
+        val radioOptions = listOf("A", "B", "C")
+        val (selectedOption, onOptionSelected) = +state { radioOptions[0] }
+        RadioGroup(
+            options = radioOptions,
+            selectedOption = selectedOption,
+            onSelectedChange = onOptionSelected
+        )
+    }
+
+}
