74066a6eb078288b530ca357b5535d70cc2a9ff7
Jens Klingenberg <mail@jensklingenberg.de>
2019-11-23 23:58:30 +0100

    update to Kotlin 1.3.60

---
+++ compose/src/main/java/de/jensklingenberg/jetpackcomposeplayground/ui/samples/mysamples/layout/HorizontalScrollerDemo.kt
@@ -0,0 +1,38 @@
+@file:Suppress("KDocUnresolvedReference")
+
+package de.jensklingenberg.jetpackcomposeplayground.ui.samples.mysamples.layout
+
+import androidx.compose.Composable
+import androidx.compose.unaryPlus
+import androidx.ui.core.Text
+import androidx.ui.foundation.HorizontalScroller
+import androidx.ui.layout.FlexColumn
+import androidx.ui.layout.Row
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.themeTextStyle
+
+@Composable
+        /**
+         * @see wiki [https://github.com/Foso/Jetpack-Compose-Playground/wiki/HorizontalScroller]
+         */
+fun HorizontalScrollerDemo() {
+
+    MaterialTheme {
+        FlexColumn {
+            HorizontalScrollerExample()
+        }
+    }
+
+}
+
+@Composable
+fun HorizontalScrollerExample() {
+    HorizontalScroller {
+        //Only one child is allowed in a VerticalScroller
+        Row {
+            for (i in 0..100) {
+                Text(text = "$i Hello World!", style = +themeTextStyle { body1 })
+            }
+        }
+    }
+}
